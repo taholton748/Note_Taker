4d5636900e993b93dbdb8ae8df22907171eaa9ed
Medici <107539009+taholton748@users.noreply.github.com>
2022-10-10 20:18:41 -0600

    added html routes and Heroku

---
+++ Develop/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+const [ notes ]= require('./db/db.json');
+
+const { v4: uuidv4 } = require('uuid'); // How to use this package
+uuidv4();
+
+app.get('/api/notes', (req,res) => {
+    res.json(notes);
+});
+app.get('/notes', (req,res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+app.listen(3001, () => {
+    console.log('API server now on port 3001!');
+});
